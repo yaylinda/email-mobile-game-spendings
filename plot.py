822f1f0860795e04afeedb26aa6b3650c716bd3e
Linda Zheng <lindazheng1993@gmail.com>
2024-01-01 23:52:20 -0600

    plot looking better

--- plot.py
+++ plot.py
@@ -1,5 +1,6 @@
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
+from matplotlib import ticker
 
 
 def plot(filename: str):
@@ -13,22 +14,21 @@ def plot(filename: str):
     # Aggregating data to get annual sum for each category
     aggregated_data = data.groupby(['year', 'category']).agg(
         {'price_with_tax': 'sum'}
-        ).reset_index()
+    ).reset_index()
     
     # Pivoting the data for the bar chart
     pivot_data = aggregated_data.pivot(
         index='year',
         columns='category',
         values='price_with_tax'
-        ).fillna(0)
+    ).fillna(0)
     
     # Ensuring the 'Other' category is last
     pivot_data = pivot_data[
-        sorted(pivot_data.columns, key=lambda x: (x != 'Other', x))]
+        sorted(pivot_data.columns, key=lambda x: (x == 'Other', x))]
     
     # Choosing a colormap with more distinguishable colors
-    # color_map = plt.cm.get_cmap('tab20', len(aggregated_data.columns))
-    color_map = plt.colormaps['tab20']
+    color_map = plt.colormaps['Accent']
     
     # Creating a stacked bar chart
     ax = pivot_data.plot(
@@ -36,17 +36,19 @@ def plot(filename: str):
         stacked=True,
         figsize=(12, 8),
         color=[color_map(i) for i in range(len(pivot_data.columns))]
-        )
+    )
     
     # Hide the right and top spines
     ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_visible(False)
     ax.spines['top'].set_visible(False)
+    ax.yaxis.grid(True, linestyle='dotted')
     
     # Calculating the sum of spendings for each category
     category_sums = pivot_data.sum(axis=0)
     
     # Creating custom labels for the legend with category names and their respective total spendings
-    legend_labels = [f"{cat}: ${total:.2f}" for cat, total in
+    legend_labels = [f"{cat} (${total:.2f})" for cat, total in
                      category_sums.items()]
     
     # Creating and adjusting the legend within the plot area
@@ -56,7 +58,7 @@ def plot(filename: str):
         loc='upper center',
         ncol=2,
         bbox_to_anchor=(0.5, 0.95)
-        )
+    )
     plt.setp(legend.get_texts(), fontsize=12)
     
     # Adjusting x-axis labels (years)
@@ -65,27 +67,33 @@ def plot(filename: str):
         fontsize=12,
         fontweight='bold',
         rotation=0
-        )
+    )
     for label in ax.get_xticklabels():
         label.set_y(
-            label.get_position()[1] - 0.03
-            )  # Adjusting the y-position of the labels for spacing
+            # Adjusting the y-position of the labels for spacing
+            label.get_position()[1] - 0.01
+        )
+    
+    ax.yaxis.set_major_formatter(
+        ticker.FuncFormatter(lambda x, pos: '' if pos == 0 else f'${x:0.0f}')
+    )
     
     # Adjusting plot elements
     plt.title(
         'Mobile Game Spendings by Year (2014 - 2023)',
-        fontsize=16,
+        fontsize=20,
         fontweight='bold'
-        )
+    )
     plt.xlabel('')
-    plt.ylabel('Amount Spent (USD)')
+    plt.ylabel('')
+    plt.yticks(fontsize=12)
     plt.tick_params(axis='x', which='both', length=0)
+    plt.tick_params(axis='y', which='both', length=0)
     plt.tight_layout()
     
     # Show the plot
     plt.show()
-    
+
 
 if __name__ == '__main__':
     plot('data.csv')
-    
